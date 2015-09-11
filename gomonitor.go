db49d43d16bea8d4c96d5413e756dd57ccd9158a
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2015-09-11 13:37:00 +0200

    [DOC] create godoc

--- gomonitor.go
+++ gomonitor.go
@@ -1,20 +1,10 @@
-package gomonitor
-
-import (
-	"fmt"
-
-	"github.com/gin-gonic/gin"
-	mon "gopkg.in/mcuadros/go-monitor.v1"
-	"gopkg.in/mcuadros/go-monitor.v1/aspects"
-)
-
-// Metrics exposes metrics of the famous
-// https://github.com/gopkg.in/mcuadros/go-monitor.v1 package to your
-// [gin](https://github.com/gin-gonic/gin) based webapp.  It supports
-// custom metrics which are not triggered using the middleware
-// context. If you want to add a page counter please see the example.
-// Metrics() get a port to expose monitoring data to and a slice of
-// aspects.Aspect defined by the user.
+// Package gomonitor is a Gin middleware, that exposes metrics to an
+// http monitor endpoint.  As base it uses
+// https://gopkg.in/mcuadros/go-monitor.v1 to expose metrics. It
+// supports custom metrics which are not triggered using the
+// middleware context. If you want to add a page counter please see
+// the example. You can even create your own aspects like defined in
+// the https://gopkg.in/mcuadros/go-monitor.v1/aspects package.
 //
 // Example:
 //    package main
@@ -47,6 +37,32 @@ import (
 //    	//..
 //    	router.Run(":8080")
 //    }
+package gomonitor
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	mon "gopkg.in/mcuadros/go-monitor.v1"
+	"gopkg.in/mcuadros/go-monitor.v1/aspects"
+)
+
+// Metrics function exposes metrics of
+// https://github.com/gopkg.in/mcuadros/go-monitor.v1 package of your
+// https://github.com/gin-gonic/gin based webapp. Metrics() get a
+// port number as parameter to expose monitoring data to and a slice
+// of aspects.Aspect defined by the user.
+//
+// Example:
+//    	router := gin.New()
+//    	counterAspect := &ginmon.CounterAspect{0}
+//    	asps := []aspects.Aspect{counterAspect}
+//    	// curl http://localhost:9000/Counter
+//    	router.Use(ginmon.CounterHandler(counterAspect))
+//    	// curl http://localhost:9000/
+//    	router.Use(gomonitor.Metrics(9000, asps))
+//    	// last middleware
+//    	router.Use(gin.Recovery())
 func Metrics(port int, asps []aspects.Aspect) gin.HandlerFunc {
 	var monitor *mon.Monitor = mon.NewMonitor(fmt.Sprintf(":%d", port))
 	for _, aspect := range asps {
