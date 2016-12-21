baa74b63ffabd7d7f0f0a13efa8b76699e2e6784
Sandor Szuecs <sandor.szuecs@zalando.de>
2016-12-21 17:20:57 +0100

    make sure we match the assertEqual signature

--- aspects/counter_test.go
+++ aspects/counter_test.go
@@ -37,7 +37,7 @@ func Test_Inc(t *testing.T) {
 		code: 404,
 	}
 	ca.reset()
-	if assert.Equal(t, ca.RequestsSum, expect, "Incrementation of counter does not work, expect %d but got %d %s",
+	if assert.Equal(t, expect, ca.RequestsSum, "Incrementation of counter does not work, expect %d but got %d %s",
 		expect, ca.RequestsSum, ballotX) {
 		t.Logf("Incrementation of counter works, expect %d and git %d %s",
 			expect, ca.RequestsSum, checkMark)
@@ -53,7 +53,7 @@ func Test_GetStats(t *testing.T) {
 
 	newCa := ca.GetStats().(CounterAspect)
 	expect := 0
-	if assert.Equal(t, newCa.RequestsSum, expect, "Return of Getstats() does not work, expect %d but got %d %s",
+	if assert.Equal(t, expect, newCa.RequestsSum, "Return of Getstats() does not work, expect %d but got %d %s",
 		expect, newCa.RequestsSum, ballotX) {
 		t.Logf("Return of Getstats() works, expect %d and got %d %s",
 			expect, newCa.RequestsSum, checkMark)
@@ -63,12 +63,12 @@ func Test_GetStats(t *testing.T) {
 		path: testpath,
 		code: 404,
 	}
-	if assert.Equal(t, newCa.RequestsSum, expect, "Return of Getstats() does not work, expect %d but got %d %s",
+	if assert.Equal(t, expect, newCa.RequestsSum, "Return of Getstats() does not work, expect %d but got %d %s",
 		expect, newCa.RequestsSum, ballotX) {
 		t.Logf("Return of Getstats() works, expect %d and got %d %s",
 			expect, newCa.RequestsSum, checkMark)
 	}
-	if assert.Equal(t, newCa.Requests[testpath], expect, "Return of Getstats() does not work, expect %d but got %d %s",
+	if assert.Equal(t, expect, newCa.Requests[testpath], "Return of Getstats() does not work, expect %d but got %d %s",
 		expect, newCa.Requests[testpath], ballotX) {
 		t.Logf("Return of Getstats() works, expect %d and got %d %s",
 			expect, newCa.Requests[testpath], checkMark)
@@ -77,12 +77,12 @@ func Test_GetStats(t *testing.T) {
 	ca.reset()
 	newCa = ca.GetStats().(CounterAspect)
 	expect = 1
-	if assert.Equal(t, newCa.RequestsSum, expect, "Return of Getstats() does not work, expect %d but got %d %s",
+	if assert.Equal(t, expect, newCa.RequestsSum, "Return of Getstats() does not work, expect %d but got %d %s",
 		expect, newCa.RequestsSum, ballotX) {
 		t.Logf("Return of Getstats() works, expect %d and got %d %s",
 			expect, newCa.RequestsSum, checkMark)
 	}
-	if assert.Equal(t, newCa.Requests[testpath], expect, "Return of Getstats() does not work, expect %d but got %d %s",
+	if assert.Equal(t, expect, newCa.Requests[testpath], "Return of Getstats() does not work, expect %d but got %d %s",
 		expect, newCa.Requests[testpath], ballotX) {
 		t.Logf("Return of Getstats() works, expect %d and got %d %s",
 			expect, newCa.Requests[testpath], checkMark)
@@ -92,7 +92,7 @@ func Test_GetStats(t *testing.T) {
 func Test_Name(t *testing.T) {
 	ca := NewCounterAspect()
 	expect := "Counter"
-	if assert.Equal(t, ca.Name(), expect, "Return of counter name does not work, expect %s but got %s %s",
+	if assert.Equal(t, expect, ca.Name(), "Return of counter name does not work, expect %s but got %s %s",
 		expect, ca.Name(), ballotX) {
 		t.Logf("Return of counter name works, expect %s and got %s %s",
 			expect, ca.Name(), checkMark)
@@ -102,7 +102,7 @@ func Test_Name(t *testing.T) {
 func Test_InRoot(t *testing.T) {
 	ca := NewCounterAspect()
 	expect := false
-	if assert.Equal(t, ca.InRoot(), expect, "Expect %v but got %v %s",
+	if assert.Equal(t, expect, ca.InRoot(), "Expect %v but got %v %s",
 		expect, ca.InRoot(), ballotX) {
 		t.Logf("Expect %v and got %v %s",
 			expect, ca.InRoot(), checkMark)
@@ -123,7 +123,7 @@ func Test_CounterHandler(t *testing.T) {
 
 	router.Use(CounterHandler(ca))
 	tryRequest(router, "GET", "/")
-	if assert.Equal(t, ca.RequestsSum, expect, "Incrementation of counter does not work, expect %d but got %d %s", expect, ca.RequestsSum, ballotX) {
+	if assert.Equal(t, expect, ca.RequestsSum, "Incrementation of counter does not work, expect %d but got %d %s", expect, ca.RequestsSum, ballotX) {
 		t.Logf("CounterHandler works, expect %d and got %d %s", expect, ca.RequestsSum, checkMark)
 	}
 }
